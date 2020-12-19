878828ab542f5e1434f9abb896a5ad5f4d2801a3
jmsaylor <johnmsaylor@gmail.com>
2020-12-18 22:37:26 -0500

    Memoization

--- src/com/johnmsaylor/main/Main.java
+++ src/com/johnmsaylor/main/Main.java
@@ -1,15 +1,18 @@
 package com.johnmsaylor.main;
 import com.johnmsaylor.BubbleSort;
+import com.johnmsaylor.Memoization;
 import com.johnmsaylor.NonDivisibleSubset;
 import com.johnmsaylor.medium.*;
 
+import java.lang.reflect.Member;
 import java.util.Arrays;
+import java.util.HashMap;
 
 
 public class Main {
 
     public static void main(String[] args) {
-        BomberMan.test();
+        System.out.println(Memoization.fib(50L, new HashMap<>()));
 
     }
 
