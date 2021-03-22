f78858938ec9d37bd09636549fc839b6d19b05fe
jmsaylor <johnmsaylor@gmail.com>
2021-03-22 16:39:35 -0400

    naive solution

--- src/com/johnmsaylor/medium/ValidString.java
+++ src/com/johnmsaylor/medium/ValidString.java
@@ -1,12 +1,15 @@
 package com.johnmsaylor.medium;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class ValidString {
     public static void test() {
-        System.out.println(isValid("ddooggc"));
-        System.out.println(isValid("doggg"));
+//        System.out.println(isValid("abcdefghhgfedecba"));
+        System.out.println(isValid("ibfdgaeadiaefgbhbdghhhbgdfgeiccbiehhfcggchgghadhdhagfbahhddgghbdehidbibaeaagaeeigffcebfbaieggabcfbiiedcabfihchdfabifahcbhagccbdfifhghcadfiadeeaheeddddiecaicbgigccageicehfdhdgafaddhffadigfhhcaedcedecafeacbdacgfgfeeibgaiffdehigebhhehiaahfidibccdcdagifgaihacihadecgifihbebffebdfbchbgigeccahgihbcbcaggebaaafgfedbfgagfediddghdgbgehhhifhgcedechahidcbchebheihaadbbbiaiccededchdagfhccfdefigfibifabeiaccghcegfbcghaefifbachebaacbhbfgfddeceababbacgffbagidebeadfihaefefegbghgddbbgddeehgfbhafbccidebgehifafgbghafacgfdccgifdcbbbidfifhdaibgigebigaedeaaiadegfefbhacgddhchgcbgcaeaieiegiffchbgbebgbehbbfcebciiagacaiechdigbgbghefcahgbhfibhedaeeiffebdiabcifgccdefabccdghehfibfiifdaicfedagahhdcbhbicdgibgcedieihcichadgchgbdcdagaihebbabhibcihicadgadfcihdheefbhffiageddhgahaidfdhhdbgciiaciegchiiebfbcbhaeagccfhbfhaddagnfieihghfbaggiffbbfbecgaiiidccdceadbbdfgigibgcgchafccdchgifdeieicbaididhfcfdedbhaadedfageigfdehgcdaecaebebebfcieaecfagfdieaefdiedbcadchabhebgehiidfcgahcdhcdhgchhiiheffiifeegcfdgbdeffhgeghdfhbfbifgidcafbfcd"));
     }
 
     static String isValid(String s) {
@@ -26,22 +29,40 @@ public class ValidString {
         //all values in hashset should be s.length/unique
         //if one is +1 return YES, of more NO
 
-        boolean result = true;
 
-        final int count_limit = s.length() / set.size();
-        //already has seen diff +1
-        boolean warning = false;
+        int count_limit = s.length() / set.size();
 
-        for (Character key : set.keySet()) {
-            if (set.get(key) > count_limit) {
-                if(set.get(key) > count_limit + 1 || warning) {
-                    result = false;
-                    break;
-                } else {
-                    warning = true;
-                }
+        if (count_limit == 1) {
+            if((s.length() + 1) / set.size() == 2) {
+                count_limit = 2;
             }
         }
+
+        ArrayList<Integer> res = new ArrayList<>();
+
+        for (Character c : set.keySet()) {
+            if (set.get(c) != count_limit) {
+                res.add(set.get(c));
+            }
+            //break if res is greater than 1
+        }
+        System.out.println(s.length() + " " + set.size());
+        System.out.println(set);
+
+        boolean result = false;
+
+        if (res.size() == 0) {
+            result = true;
+        }
+
+        if (count_limit == 2 && res.size() == 1 && res.get(0) == 1) {
+            result = true;
+        }
+
+        if (res.size() == 1 && res.get(0) - count_limit == 1) {
+            result = true;
+        }
+
 
         return result ? "YES" : "NO";
     }
