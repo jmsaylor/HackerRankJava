3c076a0f9eaf536f7ac72799d1b8b17b68200077
jmsaylor <johnmsaylor@gmail.com>
2021-03-22 14:58:14 -0400

    efficient solution

--- src/com/johnmsaylor/medium/Fairness.java
+++ src/com/johnmsaylor/medium/Fairness.java
@@ -10,44 +10,15 @@ public class Fairness {
     }
 
     static int maxMin(int k, int[] arr) {
-        //sort array
-
         Arrays.sort(arr);
 
-        System.out.println(Arrays.toString(arr));
+        int fairness = Integer.MAX_VALUE;
 
-        // calculate fairness of each window and log in table
-        OptionalInt scanMin = Arrays.stream(Arrays.copyOfRange(arr, 0, k)).min();
-        OptionalInt scanMax = Arrays.stream(Arrays.copyOfRange(arr, 0, k)).max();
-
-        int min = scanMin.getAsInt();
-
-        int max = scanMax.getAsInt();
-
-        int fairness = max - min;
-
-        for (int i = 1; i <= arr.length - k; i++) {
-            if (arr[i - 1] == min) {
-                scanMin = Arrays.stream(Arrays.copyOfRange(arr, i, (i + k))).min();
-                min = scanMin.getAsInt();
-            } else if (arr[i - 1] == max) {
-                scanMax = Arrays.stream(Arrays.copyOfRange(arr, i, (i + k))).max();
-                max = scanMax.getAsInt();
+        for (int i = 0; i <= arr.length - k; i++) {
+            if (arr[(i + k) - 1] - arr[i] < fairness) {
+                fairness = arr[(i + k) - 1] - arr[i];
             }
-
-            if (arr[(i + k) - 1] > max) {
-                max = arr[(i + k) - 1];
-            } else if (arr[(i + k) - 1] < min) {
-                min = arr[(i + k) - 1];
-            }
-
-            if ((max - min) < fairness) {
-                fairness = (max - min);
-            }
-
-
         }
-
 
         return fairness;
     }
---
+++ src/com/johnmsaylor/medium/ValidString.java
@@ -0,0 +1,18 @@
+package com.johnmsaylor.medium;
+
+public class ValidString {
+    public static void test() {
+        System.out.println(isValid("dogg"));
+        System.out.println(isValid("doggg"));
+    }
+
+    static String isValid(String s) {
+        //count the characters in a hashset
+
+        //determine total unique keys
+
+        //all values in hashset should be s.length/unique
+        //if one is +1 return YES, of more NO
+
+    }
+}
