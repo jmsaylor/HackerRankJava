cb4ffd1180ec1fc300b797acb3309342ebbfb320
jmsaylor <johnmsaylor@gmail.com>
2021-03-23 13:57:36 -0400

    final solution

--- src/com/johnmsaylor/medium/MinimumSwaps.java
+++ src/com/johnmsaylor/medium/MinimumSwaps.java
@@ -9,30 +9,21 @@ public class MinimumSwaps {
     }
 
     static int minimumSwaps(int[] arr) {
-        //Using Selection sort to find minimum number of swaps
+
         int swapCounter = 0;
+        int index = 0;
 
-        int min;
-        int minIndex = 0;
-        int swap;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == i + 1) continue;
-            min = arr[i];
-            for (int j = (i + 1); j < arr.length; j++) {
-                if (arr[j] < min) {
-                    min = arr[j];
-                    minIndex = j;
-                }
-            }
-
-            if (min < arr[i]) {
+        while (index < arr.length) {
+            if (arr[index] > index + 1) {
+                int correctLocation = arr[index] - 1;
+                int temp = arr[correctLocation];
+                arr[correctLocation] = arr[index];
+                arr[index] = temp;
                 swapCounter++;
-                swap = arr[minIndex];
-                arr[minIndex] = arr[i];
-                arr[i] = swap;
 //                System.out.println(Arrays.toString(arr));
-            }
+            } else index++;
         }
+
 
         return swapCounter;
     }
