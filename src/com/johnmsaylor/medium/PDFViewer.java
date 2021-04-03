7cf19c47b2a8c4e971e24feacb36fbc73b60722d
jmsaylor <johnmsaylor@gmail.com>
2021-04-03 19:24:32 -0400

    refactor and optimization

--- src/com/johnmsaylor/medium/PDFViewer.java
+++ src/com/johnmsaylor/medium/PDFViewer.java
@@ -2,14 +2,10 @@ package com.johnmsaylor.medium;
 
 public class PDFViewer {
     static int designerPdfViewer(int[] h, String word) {
-        int[] asci = new int[word.length()];
-        char[] charArray = word.toCharArray();
-
         int maxHeight = 0;
 
-        for (int i = 0; i < charArray.length; i++) {
-            asci[i] = (int) charArray[i] - 97;
-            maxHeight = Math.max(maxHeight, h[charArray[i] - 97]);
+        for (char c : word.toCharArray()) {
+            maxHeight = Math.max(maxHeight, h[(int) c - 97]);
         }
 
         return maxHeight * word.length();
