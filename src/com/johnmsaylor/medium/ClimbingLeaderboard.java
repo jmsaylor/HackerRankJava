1f18271c937e4333c439b6cba058a44db4fb6719
John <johnmsaylor@gmail.com>
2021-04-29 11:00:17 -0400

    misguided attempt

--- src/com/johnmsaylor/medium/ClimbingLeaderboard.java
+++ src/com/johnmsaylor/medium/ClimbingLeaderboard.java
@@ -5,24 +5,73 @@ import java.util.stream.Collectors;
 
 public class ClimbingLeaderboard {
 
-    public static List<Integer> climbingLeaderboard(List<Integer> ranked, List<Integer> player) {
-        // Write your code here
-        TreeSet<Integer> leaderboard = new TreeSet<>(ranked);
-        List<Integer> ranks = new ArrayList<>(leaderboard);
-        List<Integer> result = new ArrayList<>();
-        int rank = leaderboard.size() + 1;
-        int marker = 0;
-
-        for (int score : player) {
-            for (int i = marker; i < ranks.size(); i++) {
-                if (score >= ranks.get(i)) {
-                    rank = ranks.size() - i;
-                    marker = i;
-                } else break;
-            }
-            result.add(rank);
+    public static void main(String[] args) {
+        List<Integer> ranked = new ArrayList<>();
+        int[] t = new int[]{100, 100, 50, 40, 40, 20, 10};
+        for (int r : t) {
+            ranked.add(r);
         }
-        return result;
 
+        List<Integer> player = new ArrayList<>();
+        int[] z = new int[]{2, 25, 50, 120};
+        for (int x : z) {
+            player.add(x);
+        }
+
+        climbingLeaderboard(ranked, player);
+    }
+
+    public static List<Integer> climbingLeaderboard(List<Integer> ranked, List<Integer> player) {
+        //ranked - descending, player - ascending
+        if (player.isEmpty()) return new ArrayList<>();
+        List<Integer> resultRanks = new ArrayList<>(player.size());
+
+//        Deque<Integer> rankedDeque = new ArrayDeque<>(ranked.size());
+//        rankedDeque.addAll(player);
+        Deque<Integer> playerDeque = new ArrayDeque<>(player.size());
+        playerDeque.addAll(player);
+
+        Integer currentPlayer = playerDeque.pollLast();
+        int currentRank = 1;
+        int previous = -1;
+
+
+        for (int rank : ranked) {
+            if (rank == previous && currentPlayer < rank) continue;
+
+            if (rank > currentPlayer) {
+                previous = rank;
+                currentRank++;
+                continue;
+            }
+
+            resultRanks.add(currentRank);
+
+            if (currentPlayer != previous) {
+                previous = currentPlayer;
+                currentRank++;
+            }
+
+            currentPlayer = playerDeque.pollLast();
+
+            if (currentPlayer == null) break;
+        }
+
+        while (currentPlayer != null) {
+            resultRanks.add(currentRank);
+            if (currentPlayer == previous) {
+                continue;
+            }
+            currentRank++;
+            previous = currentPlayer;
+            currentPlayer = playerDeque.pollLast();
+        }
+
+
+
+
+        Collections.reverse(resultRanks); //might need more efficient data structure for this
+        System.out.println(resultRanks);
+        return resultRanks;
     }
 }
